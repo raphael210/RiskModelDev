25c8fb591846cfcf0009fd996dabdc06ad9b17ca
QuantAndrew <taoaming1001@163.com>
2016-01-04 16:44:00 +0800

    commit RiskModelDev.R

--- RiskModelDev.R
+++ RiskModelDev.R
@@ -1,51 +1,70 @@
-library(RFactorModel)
-library(lubridate)
-library(reshape2)
-library(plyr)
-library(stringr)
+#load packages
+suppressMessages(library(RFactorModel))
+suppressMessages(library(lubridate))
+suppressMessages(library(reshape2))
+suppressMessages(library(plyr))
+suppressMessages(library(stringr))
 
 
-
-# this function is designed to add a new index to local database
-add.index <- function(indexID="000985"){
+#' add.index.lcdb
+#'
+#' Add a index to local database
+#' @author Andrew Dow
+#' @param indexID is the indexID
+#' @return nothing
+#' @examples 
+#' add.index.lcdb(indexID="000985.SH")
+add.index.lcdb <- function(indexID="000985.SH"){
+  #check whether the index in local db
+  qr <- paste("select * from SecuMain where ID="
+              ,str_c("'EI",str_sub(indexID,1,6),"'"),sep="")
+  re <- dbGetQuery(db.local(),qr)
+  if(nrow(re)>0) return("Already in local database!")
+  
   #part 1 update local SecuMain
-  qr <- "select * from dbo.SecuMain
-  WHERE SecuCode='000985'
-  and SecuCategory=4"
+  qr <- paste("select ID,InnerCode,CompanyCode,SecuCode,SecuAbbr,SecuMarket,  
+              ListedSector,ListedState,JSID 'UpdateTime',SecuCode 'StockID_TS',
+              SecuCategory,ListedDate,SecuCode 'StockID_wind'
+              from SecuMain WHERE SecuCode=",
+              str_c("'",str_sub(indexID,start = 1,end = 6),"'"),
+              " and SecuCategory=4",sep='')
   re <- sqlQuery(db.jy(),qr)
-  tmp <- dbGetQuery(db.local(),"select * from SecuMain where ID='EI000300'")
-  re <- transform(re,ID='EI000985',SecuCode=str_pad(SecuCode,6,pad='0'),
-                  StockID_TS='SH000985',StockID_wind='000985.SH',
-                  ListedDate=rdate2int(ListedDate),UpdateTime=JSID)
-  re <- re[,colnames(tmp)]
+  re <- transform(re,ID=str_c("EI",str_sub(indexID,1,6)),
+                  SecuCode=str_pad(SecuCode,6,pad='0'),
+                  StockID_TS=str_c(str_sub(indexID,8,-1),str_sub(indexID,1,6)),
+                  StockID_wind=indexID,
+                  ListedDate=rdate2int(ListedDate))
   dbWriteTable(db.local(),"SecuMain",re,overwrite=FALSE,append=TRUE,row.names=FALSE)
   
   #part 2 update local LC_IndexComponent
-  library(timeDate)
-  library(WindR)
-  w.start()
+  suppressMessages(library(timeDate))
+  suppressMessages(library(WindR))
+  w.start(showmenu = F)
   dates <- seq.Date(as.Date('2004-12-01'),Sys.Date(),by="6 month")
   dates <- as.Date(timeNthNdayInMonth(dates, nday = 5, nth = 2, format = "%Y-%m-%d"))
   dates <- trday.nearby(dates,-1)
   
   subfun <- function(date){
-    tmp<-w.wset('SectorConstituent',date=date,windcode='000985.SH')$Data
+    tmp<-w.wset('SectorConstituent',date=date,windcode=indexID)$Data
     return(tmp)
   }
-  result<-ldply(dates,subfun)
-  result <- ddply(result, "wind_code", summarise,indate=min(date),outdate=max(date))
-  dates <- data.frame(outdate=dates,new=c(dates[2:length(dates)],NA))
-  result <- merge(result,dates,by = 'outdate')
-  result <- transform(result,indexID=c('EI000985'),Flag=ifelse(is.na(new),1,0),UpdateTime=Sys.time())
-  result <- result[,c("indexID","wind_code","indate","new","Flag","UpdateTime")]
+  oridata<-ldply(dates,subfun)
+  oridata$date <- w.asDateTime(oridata$date,asdate = T)
+  result <- ddply(oridata, "wind_code", summarise,InDate=min(date),OutDate=max(date))
+  dates <- data.frame(OutDate=dates,new=c(dates[2:length(dates)],NA))
+  result <- merge(result,dates,by = 'OutDate')
+  result <- transform(result,indexID=c(str_c("EI",str_sub(indexID,1,6))),
+                      Flag=ifelse(is.na(new),1,0),UpdateTime=Sys.time())
+  result <- result[,c("indexID","wind_code","InDate","new","Flag","UpdateTime")]
   colnames(result) <- c("indexID","SecuID","InDate","OutDate","Flag","UpdateTime")
   result <- transform(result,SecuID=str_sub(str_c('EQ',SecuID,sep = ''),start = 1,end = 8),
                       InDate=rdate2int(InDate),OutDate=rdate2int(OutDate))
   dbWriteTable(db.local(),"LC_IndexComponent",result,overwrite=FALSE,append=TRUE,row.names=FALSE)
+  return("Done!")
 }
 
 
-creat_liquid_factor <- function(type=c("update","new")){
+creat_liquid_factor <- function(type=c("new","update")){
   time <- Sys.time()
   type <- match.arg(type)
   if(type=='new'){
@@ -92,7 +111,7 @@ gf.liquidity <- function(TS){
 
 RebDates <- getRebDates(as.Date('2012-01-31'),as.Date('2014-12-31'),'month')
 TS <- getTS(RebDates,'EI000985')
-system.time(TSF <- getTSF(TS, gf.float_cap, factorDir = 1,
+system.time(TSF <- getTSF(TS, gf.liquidity, factorDir = 1,
                           factorOutlier = 3, factorStd ="norm",
                           factorNA = "median",
                           sectorAttr = defaultSectorAttr()))
